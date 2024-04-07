f8269a58e69f4ab6366479a03f15e671eb373cf1
RomualdAv <RomualdAv>
2024-04-07 12:04:06 +0200

    Routes simples de pages et titre dynamique

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ app.set('view engine', 'twig');
 app.set('views', __dirname + '/views');
 
 
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Accueil' });
+});
+
+app.get('/page1', (req, res) => {
+  res.render('page1', { title: 'Page 1' });
+});
+
+app.get('/page2', (req, res) => {
+  res.render('page2', { title: 'Page 2' });
+});
+
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
