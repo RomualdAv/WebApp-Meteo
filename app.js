7cafe762cebdb9cc9b33d8143f523bd7f6e316a6
RomualdAv <RomualdAv>
2024-04-07 16:10:08 +0200

    Liste des pays dans la table avec un css basique

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const port = 3000;
 app.set('view engine', 'twig');
 app.set('views', path.join(__dirname, 'views'));
 
+// Middleware pour servir les fichiers statiques depuis le dossier public
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 const indexController = require(path.join(__dirname, 'controllers/indexController'));
